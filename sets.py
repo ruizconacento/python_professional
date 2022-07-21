6728fc6b9cd102d1570fa452985856dd8d94b255
Miguel Ruiz <miguel.ruizmn@gmail.com>
2022-07-21 13:34:55 -0500

    comence a ver sets

---
+++ sets.py
@@ -0,0 +1,10 @@
+#theory
+
+#how the Set is
+my_set = {1,2,3,4,} #Practice set
+print("my set = ", my_set)
+
+my_set2 = {1,1,2,2,3,3, True, False, False}
+print("my set = ", my_set2)
+
+my_set3 = {[1,2,3],1}
